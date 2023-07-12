190686e99aa2ff5a33ad05007ccbafd7956a4f1d
Judith Retzlaff <Judith.Retzlaff@gmx.net>
2023-07-12 16:45:23 +0200

    add weather display and some css

--- src/App.js
+++ src/App.js
@@ -4,16 +4,33 @@ import ActivityForm from "./components/ActivityForm/ActivityForm";
 import { uid } from "uid";
 import useLocalStorageState from "use-local-storage-state";
 import List from "./components/List/List";
+import WeatherDisplay from "./components/WeatherDisplay/WeatherDisplay";
 
 function App() {
   const [activities, setActivities] = useLocalStorageState("activities", {
     defaultValue: [
-      { id: 1, activity: "test" },
-      { id: 3, activity: "test2" },
-      { id: 3, activity: "test3" },
+      { id: 1, name: "Play Games 🎮", isForGoodWeather: "false" },
+      { id: 2, name: "Watch TV 📺", isForGoodWeather: "false" },
+      { id: 3, name: "Go to the Beach 🏖️", isForGoodWeather: "true" },
+      { id: 4, name: "Build a Snowman ☃️", isForGoodWeather: "true" },
     ],
   });
-  const [weather, setWeather] = useState();
+  const [weather, setWeather] = useState([]);
+  useEffect(() => {
+    try {
+      weatherFetch();
+    } catch {
+      setWeather([
+        {
+          location: "Europe",
+          temperature: 30,
+          condition: "🌧️",
+          isGoodWeather: false,
+        },
+      ]);
+    }
+  }, []);
+
   const isGoodWeather = weather.isGoodWeather;
   async function weatherFetch() {
     const response = await fetch("https://example-apis.vercel.app/api/weather");
@@ -21,10 +38,6 @@ function App() {
     setWeather(data);
     return data;
   }
-
-  useEffect(() => {
-    weatherFetch();
-  }, []);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -45,11 +58,18 @@ function App() {
 
   function handleAddActivity(newActivity) {
     setActivities([{ id: uid(), ...newActivity }, ...activities]);
-    console.log(activities);
+  }
+
+  function handleDeleteActivity(id) {
+    setActivities(activities.filter((activity) => activity.id !== id));
   }
 
   return (
     <div className="App">
+      <WeatherDisplay
+        emoji={weather.condition}
+        temperature={weather.temperature}
+      />
       <List
         isGoodWeather={isGoodWeather}
         activities={
@@ -57,11 +77,12 @@ function App() {
             ? activities.filter(
                 (activity) => activity.isForGoodWeather === "true"
               )
-            : activities.filter((activity) => !activity.isForGoodWeather)
+            : activities.filter(
+                (activity) => activity.isForGoodWeather === "false"
+              )
         }
+        onDeleteActivity={handleDeleteActivity}
       />
-
-      <div>{weather.location}</div>
       <ActivityForm onAddActivity={handleSubmit} />
     </div>
   );
