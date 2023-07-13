3497712960de2409883684a5f2003bcbd68ff8cf
Judith Retzlaff <Judith.Retzlaff@gmx.net>
2023-07-13 13:10:05 +0200

    refactor code

--- src/components/ActivityForm/ActivityForm.js
+++ src/components/ActivityForm/ActivityForm.js
@@ -1,13 +1,26 @@
 import "./ActivityForm.css";
 
 export default function ActivityForm({ onAddActivity }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const data = event.target.elements;
+
+    const newActivity = {
+      name: data.activity.value,
+      isForGoodWeather: data.goodWeather.checked,
+    };
+    onAddActivity(newActivity);
+
+    event.target.reset();
+    event.target.activity.focus();
+  }
   return (
     <div className="form-wrapper">
       <h2 id="form-title">Add another activity!</h2>
       <form
         className="activity-form"
         aria-labelledby="form-title"
-        onSubmit={onAddActivity}
+        onSubmit={handleSubmit}
       >
         <div className="form__section">
           <label className="form__label" htmlFor="activity">
